d8bd5dae8e4631592e57770c59ef78f063719e07
Qin An <anqin314@gmail.com>
2019-10-20 21:11:10 -0700

    functional programming of simulated example autoencoder

--- simulation_example_autoencoder.py
+++ simulation_example_autoencoder.py
@@ -1,6 +1,10 @@
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers.core import Dropout, Reshape, Dense, Activation, Flatten
+
 from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D
+
+from tensorflow.python.keras.layers import Input
+
 from tensorflow.python.keras.optimizers import Adadelta, SGD, RMSprop
 import tensorflow.python.keras.losses
 from tensorflow.python.keras.constraints import maxnorm
@@ -8,7 +12,7 @@ from tensorflow.python.keras.layers.normalization import BatchNormalization
 from tensorflow.python.keras.regularizers import l1, l2
 from tensorflow.python.keras.callbacks import EarlyStopping, History
 from tensorflow.python.keras import backend as K 
-from tensorflow.python.keras.models import model_from_json
+from tensorflow.python.keras.models import model_from_json, Model
 
 
 K.set_image_data_format('channels_last')
@@ -31,15 +35,51 @@ def example_Cov_auto():
         "interspace":50
     }
     
-    [train_X, train_Y, test_X , test_Y] =  get_simulated_dataset(parameters = simutation_parameters, 
-    train_size = 1000, test_size = 1000)
+    [train_X, train_Y, test_X , test_Y] =  get_simulated_dataset(parameters = simutation_parameters, train_size = 20000, test_size = 5000)
 
-    print(train_X.shape[1::])
+    
 
 
     
     #################################
     # build autoencoder model
+    input_img = Input(shape = (1,1000,4) )
+     
+    x = Conv2D(filters=5,kernel_size=(1,15), activation="relu", padding="same" )(input_img)
+     
+    x = MaxPooling2D(   pool_size=(1,2)   )(x)
+
+    encoded = Dense(2, activation="relu")(x)
+
+    ###
+
+    x = Conv2D(filters=5,kernel_size=(1,15), activation="relu", padding="same" )(encoded)
+
+    x = UpSampling2D( size=(1,2)  )(x)
+
+    decoded = Conv2D(filters=4,kernel_size=(1,15), activation="sigmoid", padding="same")(x)
+
+    autoencoder = Model(input_img, decoded)
+
+    encoder = Model(input_img, encoded)
+
+    autoencoder.summary()
+
+    autoencoder.compile(optimizer='adam',loss='binary_crossentropy')
+
+    history_autoencoder=autoencoder.fit(x=train_X,
+                                  y=train_X,
+                                  batch_size=10,
+                                  epochs=2,
+                                  verbose=1,
+                                  callbacks=[ History()],
+                                  validation_data=(test_X, test_X))
+
+    encoded_imgs = encoder.predict(test_X)
+    
+    print(encoded_imgs.shape)
+
+'''
 
     Cov_auto = Sequential()
     Cov_auto.add(Conv2D(filters=5,kernel_size=(1,15), activation="relu", padding="same",input_shape=(1,1000,4) ))
@@ -57,15 +97,8 @@ def example_Cov_auto():
     Cov_auto.compile(optimizer='adam',loss='binary_crossentropy')
 
     # metrics_callback=MetricsCallback(train_data=(train_X,train_Y), validation_data=(valid_X,valid_Y))
-
-    history_autoencoder=Cov_auto.fit(x=train_X,
-                                  y=train_X,
-                                  batch_size=10,
-                                  epochs=50,
-                                  verbose=1,
-                                  callbacks=[ History()],
-                                  validation_data=(test_X, test_X))
-
+'''
 
 if __name__ == "__main__":
     example_Cov_auto()
+
